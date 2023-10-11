6f9189dfd8bf9b6ca7af9204961897715602081e
Vivian Oliveres <v.oliveres@gmail.com>
2023-10-11 15:50:00 +0200

    [Scala] 1. Two sum (better solution)

--- src/main/scala/TwoSum.scala
+++ src/main/scala/TwoSum.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object TwoSum {
 
   def main(args: Array[String]): Unit = {
@@ -17,7 +19,13 @@ object TwoSum {
     }
   }
 
-  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+  /**
+   * Working Solution but not the most efficient.
+   * For each value, store its index in a map.
+   * Then check if there is a couple of (key, target-key) values in the map.
+   * Be careful of the key==target-key where we need to check if there is at least 2 index for this key.
+   *
+    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     var i = 0
     val numIndexes: Map[Int, Seq[Int]] = nums.zipWithIndex
       .groupMapReduce(_._1) {case (_, index) => Seq(index)} (_ ++ _)
@@ -27,6 +35,29 @@ object TwoSum {
         return Array(numIndexes(key).head, numIndexes(key)(1))
       else if (key != other && numIndexes.contains(other))
         return Array(numIndexes(key).head, numIndexes(other).head)
+    }
+
+    Array(-1, -1)
+  }
+  */
+
+  /**
+   * Better solution as it checks, during the indexing phase, if we already have found a solution.
+   */
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val numIndexes: mutable.Map[Int, Seq[Int]] = mutable.Map()
+    var i = 0
+    while (i < nums.length) {
+      val value = nums(i)
+      val other = target - value
+      if (value == other && numIndexes.contains(value))
+        return Array(numIndexes(value).head, i)
+      else if (value != other && numIndexes.contains(other))
+        return Array(numIndexes(other).head, i)
+      else if (!numIndexes.contains(value))
+        numIndexes(value) = Seq(i)
+
+      i = i + 1
     }
 
     Array(-1, -1)
